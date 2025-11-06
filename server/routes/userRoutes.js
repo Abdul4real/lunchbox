7d9121d9d96037850a4a8b0e721e2753bff7f26a
StellaKw <144281244+StellaKw@users.noreply.github.com>
2025-11-06 01:36:23 -0500

    Delete server directory

--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-// server/routes/userRoutes.js
-import express from "express";
-import { listUsers } from "../controllers/userController.js";
-import { requireAuth } from "../middleware/auth.js";
-
-const router = express.Router();
-router.get("/", requireAuth, listUsers);
-export default router;
