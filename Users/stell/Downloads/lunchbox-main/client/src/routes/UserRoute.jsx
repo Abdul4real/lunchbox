c9fdee03380e973bebfc569eececa99704730c47
StellaKw <144281244+StellaKw@users.noreply.github.com>
2025-11-06 16:01:43 -0500

    Delete Users/stell/Downloads/lunchbox-main directory

--- Users/stell/Downloads/lunchbox-main/client/src/routes/UserRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
-
-export default function UserRoute() {
-  const { isAuthed } = useAuth();
-  return isAuthed ? <Outlet /> : <Navigate to="/signin" replace />;
-}
