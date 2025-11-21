0079dc091c0fe3b6f9b69ed4cd6326d183198f9f
StellaKw <pstellak@my.centennialcollege.ca>
2025-11-20 19:49:00 -0500

    Updated UserRoute

--- client/src/routes/UserRoute.jsx
+++ client/src/routes/UserRoute.jsx
@@ -3,6 +3,12 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export default function UserRoute() {
-  const { isAuthed } = useAuth();
+  const { isAuthed, isReady } = useAuth();
+
+  // 🕒 While we’re restoring from storage, don't redirect yet
+  if (!isReady) {
+    return null; // or a loading spinner
+  }
+
   return isAuthed ? <Outlet /> : <Navigate to="/signin" replace />;
 }
