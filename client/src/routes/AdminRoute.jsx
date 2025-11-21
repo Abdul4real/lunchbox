4a342945255fd179988e716af511480df84f51de
StellaKw <pstellak@my.centennialcollege.ca>
2025-11-20 19:48:30 -0500

    updated AdminRoute

--- client/src/routes/AdminRoute.jsx
+++ client/src/routes/AdminRoute.jsx
@@ -3,6 +3,11 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export default function AdminRoute() {
-  const { isAdmin } = useAuth();
+  const { isAdmin, isReady } = useAuth();
+
+  if (!isReady) {
+    return null;
+  }
+
   return isAdmin ? <Outlet /> : <Navigate to="/admin/login" replace />;
 }
